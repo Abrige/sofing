db9d04f2e9c623403062676f63da73533043790a
Porfus <lucaporfiri@gmail.com>
2025-06-12 11:39:04 +0200

    ultima versione

--- progettoSofIng/src/main/java/it/unicam/cs/progettosofing/model/AccessoMarketplaceUtenteGenerico.java
+++ progettoSofIng/src/main/java/it/unicam/cs/progettosofing/model/AccessoMarketplaceUtenteGenerico.java
@@ -1,4 +1,7 @@
 package it.unicam.cs.progettosofing.model;
 
 public interface AccessoMarketplaceUtenteGenerico {
+    void visualizzaProdotti();
+    void aggiungiProdottiAlCarrello();
+    void rimuoviProdottiAlCarrello();
 }
