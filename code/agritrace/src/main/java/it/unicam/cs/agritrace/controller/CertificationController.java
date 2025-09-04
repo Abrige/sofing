7d50ceb649db43e47194d928d41c57966864e702
Falsario <leo3alfa@gmail.com>
2025-09-04 19:56:53 +0200

    Push robe
    Certification e HarvesSeason

---
+++ code/agritrace/src/main/java/it/unicam/cs/agritrace/controller/CertificationController.java
@@ -0,0 +1,33 @@
+
+package it.unicam.cs.agritrace.controller;
+
+import it.unicam.cs.agritrace.dtos.responses.CertificationsResponse;
+import it.unicam.cs.agritrace.service.CertificationsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/certifications")
+public class CertificationController {
+
+    private final CertificationsService certificationsService;
+
+    public CertificationController(CertificationsService certificationsService) {
+        this.certificationsService = certificationsService;
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<CertificationsResponse>> getAllCertifications(){
+        return ResponseEntity.ok(certificationsService.GetCertifications());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CertificationsResponse> getIdCertifications(@PathVariable int id){
+        return ResponseEntity.ok(certificationsService.GetCertificationsById(id));
+    }
+}
---
+++ code/agritrace/src/main/java/it/unicam/cs/agritrace/dtos/responses/HarvestSeasonResponse.java
@@ -0,0 +1,16 @@
+
+package it.unicam.cs.agritrace.dtos.responses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record HarvestSeasonResponse(
+        @JsonProperty("harvest_season_id")
+        Integer harvestSeasonId,
+        String name,
+        @JsonProperty("month_start")
+        Short MonthStart,
+        @JsonProperty("month_end")
+        Short MonthEnd,
+        String hemisphere,
+        String description
+) {}
